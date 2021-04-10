a19c79b2804a4bdd46db838df4caa89cf679ce82
EC2 Default User <ec2-user@ip-172-31-90-156.ec2.internal>
2021-04-10 09:05:03 +0000

    [Update]3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
   def top
   end
 
+# 下記にrb：８を記入していなかったのが原因で、新規投稿時にparameter is missing or empty errorが表示
   def index
     @books = Book.all
     @book = Book.new
@@ -17,12 +18,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = 'Book was successfully created.'
       redirect_to book_path(book.id)
     else
-      render edit_book_path(book.id)
+      @books = Book.all
+      render :index
     end
   end
 
@@ -31,12 +33,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = 'Book was successfully updated.'
       redirect_to book_path(book.id)
     else
-      render edit_book_path(book.id)
+      render :edit
     end
   end
 
